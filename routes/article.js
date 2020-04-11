4731b5e539e8d922b27eb3e2ca052c0b367e97d8
tianwei <464231692@qq.com>
2020-04-10 22:51:14 -0400

    upload

--- routes/article.js
+++ routes/article.js
@@ -2,6 +2,8 @@ let express = require('express');
 let router = express.Router();
 let Model = require('../model.js');
 let Article = Model.Article
+let multiparty = require('multiparty')
+let fs = require('fs')
 
 router.post('/add',function(req,res,next){
   let id = parseInt(req.body.id)
@@ -41,5 +43,19 @@ router.get('/delete',function(req,res,next){
   });
 })
 
+
+router.post('/upload',function(req,res,next){
+  let form = new multiparty.Form()
+  form.parse(req,function(err,fields,files){
+    let file = files.filedata[0]
+    let rs = fs.createReadStream(file.path)
+    let newPath = '/uploads/' + file.originalFilename
+    let ws = fs.createWriteStream('./public' + newPath)
+    rs.pipe(ws)
+    ws.on('close',function(){
+      res.send({err:'',msg:newPath})
+    })
+  })
+})
 
 module.exports = router;
